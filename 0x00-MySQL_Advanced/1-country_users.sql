b38cb71eae0423c0410d03e4eab6022d020d99f1
Eugene Agesa <eugenethebandit@gmail.com>
2024-10-19 10:07:48 -0700

    database created

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,10 @@
 -- Creating a tale with unique users.
 -- Script should be executed on any database
 
-CREATE TABLE IF NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'email' VARCHAR(255) NOT NULL UNIQUE,
-    'name' VARCHAR(255),
-    'country' CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
